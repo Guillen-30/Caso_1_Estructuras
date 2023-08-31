e52657b17ad049c0811940c4d5793de279dd72a3
Guillen-30 <sguillenguzman@gmail.com>
2023-08-31 17:56:24 -0600

    Correccion archivo metodosOperacion.h

--- Preliminar #1/MetodosOperacion.h
+++ Preliminar #1/MetodosOperacion.h
@@ -1,21 +1,18 @@
-/* Se usara un TAD lita simple/Array, se ordenara el array segun la relevancia que puede ser modificada, 
+/* Se usara un TAD lita doblemente enlazada, se ordenara segun la relevancia que puede ser modificada, 
 por lo tanto se ordenara antes de cualquier busqueda y para la busqueda solo se mostraran aquellos 
 titulares que contengan una o mas palabras de la lista ingresada por el usuario, ya con eso, se puede decidir 
 si eliminar la noticia o mostrarla, la fecha sera otro parametro del titular */
+
 #include "MetodosTAD.h"
 #include <string>
 
 
-class MetodosOperacion {
-public:
-    MetodosOperacion(int cap = 10);
+void agregar(titular);
 
-    void agregar(const std::string& elemento);  // Agregar un elemento al array
-    bool buscar(const std::string& elemento) const;  // Buscar un elemento en el array
-    void eliminar(const std::string& elemento);  // Eliminar un elemento del array
-    // Otros métodos para reordenar, etc.
+void eliminar(titular);
 
-private:
-    MiArray miArray;  // Usar la clase MiArray para almacenar los datos
-};
+Titular *listar();
 
+Titular *buscar(string keyWord );
+
+Titular mover(titular, int posicion);
